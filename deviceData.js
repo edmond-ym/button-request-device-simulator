7885e44cbce070431797c1ed784cff91b2fbcaab
edmond95155156 <51832514+edmond95155156@users.noreply.github.com>
2022-03-22 20:20:50 +0800

    Add files via upload

---
+++ deviceData.js
@@ -0,0 +1,15 @@
+ ////Define Everything Here
+const DeviceData=[
+    
+    {
+        device_id:"{UUID}",
+        bearer_token:"{Token}",
+        button_list:[
+            {id:"Test", nickname:"Test"},
+        ]
+    },
+   
+];
+
+const APILink="https://143.198.74.202/deviceAPI/v1";
+
