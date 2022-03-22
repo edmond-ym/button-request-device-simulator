5f0c04a766e0b2ef26202124e409df2503a16a8f
edmond95155156 <51832514+edmond95155156@users.noreply.github.com>
2022-03-22 20:41:19 +0800

    Update deviceData.js

--- config/deviceData.js
+++ config/deviceData.js
@@ -2,14 +2,14 @@
 const DeviceData=[
     
     {
-        device_id:"{UUID}",
-        bearer_token:"{Token}",
+        device_id:"(UUID)",
+        bearer_token:"(Token)",
         button_list:[
-            {id:"{Button Id}", nickname:"{Nickname}"},
+            {id:"(Button Id)", nickname:"(Nickname)"},
         ]
     },
    
 ];
 
-const APILink="{API Base Url}";
+const APILink="(API Base Url)";
 
